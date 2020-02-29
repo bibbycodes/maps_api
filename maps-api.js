6974592ef5c84724db85b5d4be90dd73bec9448c
Robert <bibbycodes@gmail.com>
2020-02-29 21:32:40 +0000

    can get directions from api

---
+++ maps-api.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const util = require('util')
+const axios = require('axios')
+
+class Directions {
+  constructor() {
+    this.base_url = "https://maps.googleapis.com/maps/api/directions/json"
+    this.dir_key = process.env.DIRECTIONS_KEY
+  }
+
+  async getDirections(origin, destination){
+    let response = await axios.get(`${this.base_url}?origin=${origin}&destination=${destination}&key=${this.dir_key}`)
+    let string = `${this.base_url}?origin=${origin}&destination=${destination}&key=${this.dir_key}`
+    console.log(string)
+    return JSON.stringify(response.data)
+  }
+}
+
+let directions = new Directions()
+
+let dir = directions.getDirections("london", "paris")
