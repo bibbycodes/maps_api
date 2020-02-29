4d8586441d1caadb513aa9b56249504485c882b0
Robert <bibbycodes@gmail.com>
2020-02-29 21:54:25 +0000

    can get travel duration from a to b

--- maps-api.js
+++ maps-api.js
@@ -4,16 +4,26 @@ const axios = require('axios')
 
 class Directions {
   constructor() {
-    this.base_url = "https://maps.googleapis.com/maps/api/directions/json"
-    this.dir_key = process.env.DIRECTIONS_KEY
+    this.baseUrl = "https://maps.googleapis.com/maps/api/directions/json"
+    this.dirKey = process.env.DIRECTIONS_KEY
   }
 
   async getDirections(origin, destination){
-    let response = await axios.get(`${this.base_url}?origin=${origin}&destination=${destination}&key=${this.dir_key}`)
-    return JSON.stringify(response.data)
+    console.log(`${this.baseUrl}?origin=${origin}&destination=${destination}&key=${this.dirKey}`)
+    let response = await axios.get(`${this.baseUrl}?origin=${origin}&destination=${destination}&key=${this.dirKey}`)
+    return response.data
+  }
+
+  async travelTime(origin, destination) {
+    let directions = await this.getDirections(origin, destination)
+    let routes = directions.routes
+    let timeSecs = 0
+    for (let i = 0; i < routes[0].legs.length; i++) {
+      timeSecs += routes[0].legs[i].duration.value
+    } 
+    return timeSecs
   }
 }
 
 let directions = new Directions()
-
-let dir = directions.getDirections("london", "paris")
+let travelTime = directions.travelTime("London", "Manchester").then(res => console.log(res))
